036f30606bd86d32496ae65640261da0fd7314d0
uo276026 <91556753+uo276026@users.noreply.github.com>
2022-03-26 14:35:03 +0100

    SDI-2122-806-7.5-Vistas y motores de plantillas

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,17 +1,33 @@
 module.exports = function(app) {
     app.get("/songs", function (req, res) {
-        let response = "";
-        if(req.query.title!=null && typeof (req.query.title)!="undefined")
-            response='Titulo: ' + req.query.title + '<br>'
-        if(req.query.author!=null && typeof(req.query.autor)!="undefined")
-            response+= 'Autor: ' + req.query.author;
+        let songs=[{
+            "title":"Blank space",
+            "price":"1.2"
+        },{
+            "title":"See you again",
+            "price":"1.1"
+        },{
+            "title":"Uptown Funk",
+            "price":"1.1"
+        }];
 
-        res.send(response);
+        let response={
+            seller:'Tienda de canciones',
+            songs:songs
+        };
+        res.render("shop.twig", response);
     });
 
     app.get('/add', function(req, res) {
         let response = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(response));
+    });
+
+    //Tiene que ir antes de /songs/:id porque si lo hacemos al reves, siempre
+    //intentemos agregar una cancion, es como si estuvieramos accediendo a la
+    //cancion cuyo id=add
+    app.get('/songs/add', function (req, res) {
+        res.render("add.twig");
     });
 
     app.get('/songs/:id', function(req, res) {
@@ -31,14 +47,17 @@ module.exports = function(app) {
             + " precio: " + req.body.price
         res.send(response);
     });
+
+
+
     //Responde a cualq peticiones que empieze por promo, no se si va aqui
     app.get('/promo*', function (req, res) {
         res.send('Respuesta al patrón promo*');
     });
+
     //Responde cualq peticiones que empiece por pro y termine en ar
     app.get('/pro*ar', function (req, res) {
         res.send('Respuesta al patrón pro*ar');
     });
-
 };
 
