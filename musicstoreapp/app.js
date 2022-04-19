e04bb605afe9aabf0ad6ea8d2165358cfd0d1cd9
uo276026 <91556753+uo276026@users.noreply.github.com>
2022-04-19 11:48:55 +0200

    SDI-2122-806-10.1-SW API REST CRUD Canciones.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -59,7 +59,7 @@ let commentsRepository = require("./repositories/commentsRepository.js");
 songsRepository.init(app, MongoClient);
 commentsRepository.init(app, MongoClient);
 require("./routes/songs.js")(app, songsRepository, commentsRepository);
-//require("./routes/songs.js")(app, commentsRepository);
+require("./routes/api/songsAPIv1.0.js")(app, songsRepository);
 require("./routes/songs.js")(app, MongoClient);
 
 require("./routes/comments.js")(app, commentsRepository);
